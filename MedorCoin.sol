54c5e85a6a2a5577d00bd8a814e713674fe76d3c
Ajvlogger <ajvlogger4@gmail.com>
2026-02-01 03:11:04 +0800

    Update MedorCoin.sol

--- MedorCoin.sol
+++ MedorCoin.sol
@@ -19,41 +19,44 @@ contract MedorCoin {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    // Events
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    // Constructor: mint fixed supply to deployer
     constructor() {
         uint256 initialSupply = 21_000_000 * 10 ** decimals;
-        totalSupply = initialSupply;
         balanceOf[msg.sender] = initialSupply;
+        totalSupply = initialSupply;
         emit Transfer(address(0), msg.sender, initialSupply);
     }
 
-    // Transfer tokens
     function transfer(address to, uint256 amount) public returns (bool) {
+        require(to != address(0), "Cannot send to zero address");
         require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+
         balanceOf[msg.sender] -= amount;
         balanceOf[to] += amount;
+
         emit Transfer(msg.sender, to, amount);
         return true;
     }
 
-    // Approve spender
     function approve(address spender, uint256 amount) public returns (bool) {
+        require(spender != address(0), "Cannot approve zero address");
+
         allowance[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
-    // Transfer tokens from another account
     function transferFrom(address from, address to, uint256 amount) public returns (bool) {
+        require(to != address(0), "Cannot send to zero address");
         require(balanceOf[from] >= amount, "Insufficient balance");
         require(allowance[from][msg.sender] >= amount, "Allowance exceeded");
-        allowance[from][msg.sender] -= amount;
+
         balanceOf[from] -= amount;
         balanceOf[to] += amount;
+        allowance[from][msg.sender] -= amount;
+
         emit Transfer(from, to, amount);
         return true;
     }
