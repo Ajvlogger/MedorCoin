e05c3b5fc1989cb916277eb3394c31b27308fc57
Ajvlogger <ajvlogger4@gmail.com>
2026-02-01 02:24:53 +0800

    Update MedorCoin.sol

--- MedorCoin.sol
+++ MedorCoin.sol
@@ -1,10 +1,69 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.31;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+/*
+    MedorCoin (MEDOR)
+    - Fixed supply: 21,000,000
+    - No owner
+    - No minting
+    - No taxes
+    - No pausing
+    - No blacklist / whitelist
+    - No proxy
+    - No external calls
+    - Immutable after deployment
+*/
+
+contract ERC20 {
+    string public name;
+    string public symbol;
+    uint8 public constant decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        require(balanceOf[from] >= amount, "Insufficient balance");
+        require(allowance[from][msg.sender] >= amount, "Allowance exceeded");
+        allowance[from][msg.sender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+        return true;
+    }
+
+    function _mint(address to, uint256 amount) internal {
+        totalSupply += amount;
+        balanceOf[to] += amount;
+        emit Transfer(address(0), to, amount);
+    }
+}
 
 contract MedorCoin is ERC20 {
     constructor() ERC20("MedorCoin", "MEDOR") {
-        _mint(msg.sender, 1_000_000 * 10 ** decimals());
+        _mint(msg.sender, 21_000_000 * 10 ** decimals);
     }
 }
